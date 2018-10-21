a36288d48e79061edef1ecefa9270e88a3141e9e
nokijp <nokijp@users.noreply.github.com>
2018-10-21 18:50:47 +0900

    modify README

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # DateUtil
 
-A command line tool to display various information about Japanese calendar, such as Japanese public holidays and Rokuyo.
+日本の暦を扱うためのコマンドラインツールです。
+
+主な機能は以下の通りです。
+
+- 祝日判定
+- 六曜計算
 
 
-## Build and Install
+## インストール方法
 
 ```bash
 $ git clone https://github.com/nokijp/date-util.git
@@ -11,19 +16,19 @@ $ cd date-util
 $ stack install 
 ```
 
-[Stack](https://www.haskellstack.org/) is required to build DateUtil.
+ビルドには [Stack](https://www.haskellstack.org/) が必要です。
+
+`~/.local/bin` にパスを通すのも忘れないでください。
 
 
-## Usage
+## 主な使い方
 
-### Checking if today is a holiday
+ここにない使い方は `--help` を参照してください。`dateutil holiday --help` のように、各サブコマンドでも確認できます。
 
-The sub-command `isholiday` returns an exit code
-
-```
-$ dateutil isholiday
-$ echo $?
-0
-```
-
-### Show the type of holiday 2000/03/20 is a holiday
+| 使い方 | 説明 |
+|---|---|
+| `dateutil holiday` | 今日が祝日であれば、その名前を表示する |
+| `dateutil holiday 2000/01/01` | 特定の日付の祝日を調べる |
+| `dateutil holiday --exit-code` | 祝日かどうかを終了コードで返す |
+| `dateutil rokuyo` | 今日の六曜を表示する |
+| `dateutil rokuyo 2000/01/01` | 特定の日付の六曜を表示する |
--- src/Data/Time/DateUtil/OptParser.hs
+++ src/Data/Time/DateUtil/OptParser.hs
@@ -9,29 +9,31 @@ import Data.Time.DateUtil.Command
 import Options.Applicative
 
 parseOpt :: [String] -> ParserResult Command
-parseOpt = execParserPure defaultPrefs parserInfo
+parseOpt = execParserPure pref parserInfo
+
+pref :: ParserPrefs
+pref = defaultPrefs { prefShowHelpOnError = True
+                    , prefShowHelpOnEmpty = True
+                    }
 
 parseOptIO :: IO Command
 parseOptIO = execParser parserInfo
 
 parserInfo :: ParserInfo Command
-parserInfo = info (parser <**> helper) $ progDesc "a tool for dealing with the Japanese calendar" <> fullDesc
+parserInfo = info (parser <**> helper) $ progDesc "A tool for dealing with the Japanese calendar" <> fullDesc
 
 parser :: Parser Command
 parser =   subparser (  command "holiday" (info ((HolidayCommand <$> holidayOptParser) <**> helper)
-                                                (progDesc "show the name of holiday if today (or the specified day) is holiday" <> fullDesc)
+                                                (progDesc "Show the name of holiday if today (or the specified day) is holiday" <> fullDesc)
                                           )
                      <> command "rokuyo" (info ((RokuyoCommand <$> rokuyoOptParser) <**> helper)
-                                               (progDesc "show the name of Rokuyo" <> fullDesc)
+                                               (progDesc "Show the name of Rokuyo" <> fullDesc)
                                          )
                      )
 
 holidayOptParser :: Parser HolidayCommandType
-holidayOptParser =   (HolidayStdOut <$> dateParser)
-                 <|> (HolidayExitCode <$> (  flag' () (long "exit-code" <> help "exit with 0 if the specified day is a holiday, otherwise exit with 1")
-                                          *> dateParser
-                                          )
-                     )
+holidayOptParser = (\c -> if c then HolidayExitCode else HolidayStdOut) <$> switch (long "exit-code" <> help "Exit with 0 if the specified day is a holiday, otherwise exit with 1")
+                                                                        <*> dateParser
 
 rokuyoOptParser :: Parser RokuyoCommandType
 rokuyoOptParser = RokuyoStdOut <$> dateParser
@@ -40,7 +42,7 @@ dateParser :: Parser (Maybe Day)
 dateParser = optional (argument (eitherReader parseDate) (metavar "DATE"))
   where
     parseDate :: String -> Either String Day
-    parseDate s = maybe (Left $ "invalid date format: " ++ s) Right $ parseResult s
+    parseDate s = maybe (Left $ "Invalid date format: " ++ s) Right $ parseResult s
     parseResult :: String -> Maybe Day
     parseResult s =   parseDateMaybe "%Y-%-m-%-d" s
                   <|> parseDateMaybe "%Y/%-m/%-d" s
